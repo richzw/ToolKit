46b101844dce41a8807c88a3a78a386e68108847
Wei Zang <richzw@gmail.com>
2021-06-05 16:22:27 +0800

    Add xmind docs

---
+++ doc/缓存设计.mm
@@ -0,0 +1,20 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1617883937355" ID="ID_708481904" MODIFIED="1617883950924" TEXT="&#x7f13;&#x5b58;&#x8bbe;&#x8ba1;">
+<node CREATED="1617883981558" ID="ID_1888289379" MODIFIED="1617883991385" POSITION="right" TEXT="&#x7f13;&#x5b58;&#x4e0d;&#x4e00;&#x81f4;">
+<node CREATED="1617883991387" ID="ID_1124710788" MODIFIED="1617884131131" TEXT="&#x5148;&#x5220;&#x9664;&#x7f13;&#x5b58;&#xff0c;&#x518d;&#x66f4;&#x65b0;&#x6570;&#x636e;&#x5e93;&#x3002;">
+<node CREATED="1617884135543" ID="ID_58516004" MODIFIED="1617884142634" TEXT="&#x89e3;&#x51b3;&#x65b9;&#x6848;&#x662f;&#x4f7f;&#x7528;&#x5ef6;&#x8fdf;&#x53cc;&#x5220;"/>
+<node CREATED="1617884146295" ID="ID_194049270" MODIFIED="1617884215110" TEXT="&#x5148;&#x5220;&#x9664;&#x7f13;&#x5b58;&#xff0c;&#x6570;&#x636e;&#x5e93;&#x8fd8;&#x6ca1;&#x6709;&#x66f4;&#x65b0;&#x6210;&#x529f;&#xff0c;&#x6b64;&#x65f6;&#x5982;&#x679c;&#x8bfb;&#x53d6;&#x7f13;&#x5b58;&#xff0c;&#x7f13;&#x5b58;&#x4e0d;&#x5b58;&#x5728;&#xff0c;&#x53bb;&#x6570;&#x636e;&#x5e93;&#x4e2d;&#x8bfb;&#x53d6;&#x5230;&#x7684;&#x662f;&#x65e7;&#x503c;&#xff0c;&#x7f13;&#x5b58;&#x4e0d;&#x4e00;&#x81f4;&#x53d1;&#x751f;&#x3002;"/>
+</node>
+<node CREATED="1617884099580" ID="ID_949712285" MODIFIED="1617884099580" TEXT=""/>
+<node CREATED="1617884008345" ID="ID_28475626" MODIFIED="1617884251591" TEXT="&#x5148;&#x66f4;&#x65b0;&#x6570;&#x636e;&#x5e93;&#xff0c;&#x518d;&#x5220;&#x9664;&#x7f13;&#x5b58;&#x3002;">
+<node CREATED="1617884253348" ID="ID_1627518141" MODIFIED="1617884257304" TEXT="&#x89e3;&#x51b3;&#x65b9;&#x6848;&#x662f;&#x6d88;&#x606f;&#x961f;&#x5217;&#x6216;&#x8005;&#x5176;&#x4ed6;binlog&#x540c;&#x6b65;&#xff0c;&#x5f15;&#x5165;&#x6d88;&#x606f;&#x961f;&#x5217;&#x4f1a;&#x5e26;&#x6765;&#x66f4;&#x591a;&#x7684;&#x95ee;&#x9898;&#xff0c;&#x5e76;&#x4e0d;&#x63a8;&#x8350;&#x76f4;&#x63a5;&#x4f7f;&#x7528;&#x3002;"/>
+<node CREATED="1617884262858" ID="ID_1023185817" MODIFIED="1617884282384" TEXT="&#x66f4;&#x65b0;&#x6570;&#x636e;&#x5e93;&#x6210;&#x529f;&#xff0c;&#x5982;&#x679c;&#x5220;&#x9664;&#x7f13;&#x5b58;&#x5931;&#x8d25;&#x6216;&#x8005;&#x8fd8;&#x6ca1;&#x6709;&#x6765;&#x5f97;&#x53ca;&#x5220;&#x9664;&#xff0c;&#x90a3;&#x4e48;&#xff0c;&#x5176;&#x4ed6;&#x7ebf;&#x7a0b;&#x4ece;&#x7f13;&#x5b58;&#x4e2d;&#x8bfb;&#x53d6;&#x5230;&#x7684;&#x5c31;&#x662f;&#x65e7;&#x503c;&#xff0c;&#x8fd8;&#x662f;&#x4f1a;&#x53d1;&#x751f;&#x4e0d;&#x4e00;&#x81f4;"/>
+</node>
+</node>
+<node CREATED="1617884314544" ID="ID_1687372506" MODIFIED="1617884318019" POSITION="left" TEXT="Source">
+<node CREATED="1617884318804" ID="ID_761372445" MODIFIED="1617884321123" TEXT="https://segmentfault.com/a/1190000038572546"/>
+</node>
+<node CREATED="1617884321745" ID="ID_344249522" MODIFIED="1617884321745" POSITION="right" TEXT=""/>
+</node>
+</map>
