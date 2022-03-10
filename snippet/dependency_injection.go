928431dce8b2ff3dbf03b835ac5bd0a0f3fdc4fa
Wei Zang <richzw@gmail.com>
2022-03-10 11:36:03 +0800

    feat(snippet): add di of fx snippet

---
+++ snippet/dependency_injection.go
@@ -0,0 +1,37 @@
+package snippet
+
+// Doc: https://pmihaylov.com/shared-components-go-microservices/
+// Source: https://github.com/preslavmihaylov/fxappexample/tree/v2-fx-example
+// fx module: https://github.com/preslavmihaylov/fxappexample/tree/v3-fx-modules
+import (
+	"go.uber.org/zap"
+	"net/http"
+)
+
+func main() {
+	fx.New(
+		fx.Provide(ProvideConfig),
+		fx.Provide(ProvideLogger),
+		fx.Provide(http.NewServeMux),
+		fx.Invoke(httphandler.New),
+		fx.Invoke(registerHooks),
+	).Run()
+}
+
+func registerHooks(
+	lifecycle fx.Lifecycle,
+	logger *zap.SugaredLogger, cfg *Config, mux *http.ServeMux,
+) {
+	lifecycle.Append(
+		fx.Hook{
+			OnStart: func(context.Context) error {
+				logger.Info("Listening on ", cfg.ApplicationConfig.Address)
+				go http.ListenAndServe(cfg.ApplicationConfig.Address, mux)
+				return nil
+			},
+			OnStop: func(context.Context) error {
+				return logger.Sync()
+			},
+		},
+	)
+}
