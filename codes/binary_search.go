36921ac1cc1c9baa44d66c65bd0a1ef26d0b9a88
Wei Zang <richzw@gmail.com>
2022-04-19 20:11:23 +0800

    feat(codes): add binary search

---
+++ codes/binary_search.go
@@ -0,0 +1,55 @@
+package codes
+
+func bsearch(nums []int, target int) int {
+	var left = 0
+	var right = len(nums) - 1
+
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
+
+func left_bound(nums []int, target int) int {
+	var left = 0
+	var right = len(nums)
+
+	for left < right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			right = mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return left
+}
+
+func right_bound(nums []int, target int) int {
+	var left = 0
+	var right = len(nums)
+
+	for left < right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			left = mid + 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return left - 1
+}
